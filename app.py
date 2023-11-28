6eb108395517bb27aaef14bccf5d8838cff9fb59
procodingclass <88050205+procodingclass@users.noreply.github.com>
2023-11-28 14:01:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,4 +20,4 @@ app = create_app()
 app.register_blueprint(blueprint, url_prefix='')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug = True, port=3000)
+    app.run(host="0.0.0.0", debug = True, port=10000)
